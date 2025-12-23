81dfea810d8068bdefdb18b29e5f93005db9d6ad
Janvisrashtasoft <janvichovatiya.srashtasoft@gmail.com>
2025-12-23 11:53:04 +0530

    feat: Enable TSX compilation, update Checkbox component styling, and implement local storage path resolution for media assets.

--- libraries/react-shared-libraries/src/form/checkbox.tsx
+++ libraries/react-shared-libraries/src/form/checkbox.tsx
@@ -44,8 +44,11 @@ export const Checkbox = forwardRef<
     }
   }, [val]);
   return (
-    <div className="flex gap-[10px]">
+    <div className="flex gap-[10px] text-textColor">
       <div
+        style={{
+          backgroundColor: '#612ad5', border: '1px solid #612ad5'
+        }}
         ref={ref}
         {...disableForm ? {} : form.register(props.name!)}
         onClick={changeStatus}
@@ -53,7 +56,7 @@ export const Checkbox = forwardRef<
           'cursor-pointer rounded-[4px] select-none w-[24px] h-[24px] justify-center items-center flex text-white',
           variant === 'default' || !variant
             ? 'bg-forth'
-            : 'border-customColor1 border-2 bg-customColor2',
+            : 'border-customColor1 border-2 border-solid bg-customColor2',
           className
         )}
       >
--- libraries/react-shared-libraries/src/helpers/use.media.directory.ts
+++ libraries/react-shared-libraries/src/helpers/use.media.directory.ts
@@ -1,8 +1,18 @@
 import { useCallback } from 'react';
+import { useVariables } from '@gitroom/react/helpers/variable.context';
+
 export const useMediaDirectory = () => {
+  const { storageProvider, backendUrl } = useVariables();
+
   const set = useCallback((path: string) => {
+    if (storageProvider === 'local' && path.startsWith('http')) {
+      return path;
+    }
+    if (storageProvider === 'local' && !path.startsWith('http')) {
+      return backendUrl + '/uploads' + path;
+    }
     return path;
-  }, []);
+  }, [storageProvider, backendUrl]);
   return {
     set,
   };
